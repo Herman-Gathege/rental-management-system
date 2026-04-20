449b660bc0cc9b40f3e5ebd4cdd30b9d05cd1322
Herman-Gathege <172432295+Herman-Gathege@users.noreply.github.com>
2026-04-20 11:25:41 +0300

    users can login

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -6,6 +6,9 @@ from app.models.users import User
 from app.models.organization import Organization
 from app.schemas.user import UserRegister
 from app.core.security import hash_password
+from app.schemas.user import UserLogin
+from app.core.security import verify_password
+from app.core.jwt import create_access_token
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -32,3 +35,21 @@ def register(user: UserRegister, db: Session = Depends(get_db)):
     db.commit()
 
     return {"message": "User registered successfully"}
+
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    if not verify_password(user.password, db_user.password_hash):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    token = create_access_token({"sub": db_user.id})
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
